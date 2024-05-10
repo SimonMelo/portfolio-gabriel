a738239a4894be01f893e375cba02e66597fa330
Gabriel Melo <123557291+SimonMelo@users.noreply.github.com>
2024-05-10 20:28:19 +0000

    feat: finish the about section and add application section

--- portfolio/src/app/components/NavBar.js
+++ portfolio/src/app/components/NavBar.js
@@ -5,15 +5,15 @@ const Navbar = () => {
         <nav className="bg-gray-800 py-4">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
                 {/* Logo */}
-                <div className="flex-shrink-0">
+                {/* <div className="flex-shrink-0">
                     <img src="/path/to/your/logo.png" alt="Logo" className="h-8" />
-                </div>
+                </div> */}
                 {/* Links */}
                 <div className="flex justify-end w-full">
-                    <a href="#inicio" className="text-white font-bold hover:text-gray-300 mr-4">Início</a>
-                    <a href="#sobre-mim" className="text-white font-bold hover:text-gray-300 mr-4">Sobre Mim</a>
-                    <a href="#projetos" className="text-white font-bold hover:text-gray-300 mr-4">Projetos</a>
-                    <a href="#contato" className="text-white font-bold hover:text-gray-300">Contato</a>
+                    <a href="#inicio" className="text-white font-bold hover:text-red-300 mr-4">Início</a>
+                    <a href="#sobre-mim" className="text-white font-bold hover:text-red-300 mr-4">Sobre Mim</a>
+                    <a href="#projetos" className="text-white font-bold hover:text-red-300 mr-4">Aplicações</a>
+                    <a href="#contato" className="text-white font-bold hover:text-red-300">Contato</a>
                 </div>
             </div>
         </nav>
--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -26,18 +26,95 @@ const Home = () => {
           <div id="about-me">
             <h2 id="about-title">Sobre mim</h2>
             <p id="p-me">
-              Me chamo Gabriel Melo e sou um estudante de Análise e Desenvolvimento de Sistemas (ADS) na Unit. Durante um ano, tive a oportunidade de atuar como Desenvolvedor Frontend no banco digital <a href="https://www.delbank.com.br" target="_blank" id="delbank-name">Delbank</a>. Nessa experiência pude trabalhar em diversos projetos, frameworks e linguagens.
-              <br/><br/>
-              Atualmente, estou em busca de aperfeiçoamento em minha stack de desenvolvimento para me tornar um desenvolvedor FullStack. Acredito na importância de adquirir conhecimento em todas as camadas de uma aplicação, desde o frontend até o backend, para criar soluções mais completas e eficientes.
+              Me chamo Gabriel Melo e sou um estudante de Análise e Desenvolvimento de Sistemas (ADS) na Unit. Durante um ano, tive a oportunidade de atuar como Desenvolvedor
+              Frontend no banco digital <a href="https://www.delbank.com.br" target="_blank" id="delbank-name">Delbank</a>.
+              Nessa experiência pude trabalhar em diversos projetos com diversos frameworks e linguagens.
+              <br /><br />
+              Atualmente, estou em busca de aperfeiçoamento em minha stack de desenvolvimento para me tornar um desenvolvedor FullStack. Acredito na importância de adquirir
+              conhecimento em todas as camadas de uma aplicação, desde o frontend até o backend, para criar soluções mais completas e eficientes.
             </p>
           </div>
-          <div id="text-area">
-            
+          <div id="skills" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div class="col-span-full mb-4">
+              <h2 id="title-skills" class="text-center font-bold">Principais Tecnologias</h2>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">Javascript</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">NodeJS</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">ReactJS</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">NextJS</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">VueJS</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">Postman</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">ExpressJS</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">NestJS</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">.NET</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">MySQL</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">SQLite</p>
+            </div>
+            <div class="skills-item bg-gray-100 p-4 rounded-md transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+              <p class="text-center font-bold">GIT</p>
+            </div>
           </div>
         </div>
       </section>
       <section id="projetos">
-        <h2>Projetos</h2>
+        <div id="project-section" className="p-4">
+          <h2 className="text-3xl font-bold text-center mt-12 mb-8" id="title-project">Aplicações</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" id="div-projects">
+            {/* Primeira linha */}
+            <div className="flex flex-col items-center justify-center bg-white p-2 sm:p-4 rounded-lg shadow-md div-proj mx-auto">
+              <div className="w-16 h-16 rounded-full mb-4"></div>
+              <p className="text-center text-sm">Frontend de um formulário de pesquisa</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center bg-white p-2 sm:p-4 rounded-lg shadow-md div-proj mx-auto">
+              <div className="w-16 h-16 rounded-full mb-4"></div>
+              <p className="text-center text-sm">API de formulário de pesquisa</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center bg-white p-2 sm:p-4 rounded-lg shadow-md div-proj mx-auto">
+              <div className="w-16 h-16 rounded-full mb-4"></div>
+              <p className="text-center text-sm">Frontend de um sistema de jornada trabalhista</p>
+            </div>
+
+            {/* Segunda linha */}
+            <div className="flex flex-col items-center justify-center bg-white p-2 sm:p-4 rounded-lg shadow-md div-proj mx-auto">
+              <div className="w-16 h-16 rounded-full mb-4"></div>
+              <p className="text-center text-sm">API de um sistema de jornada trabalhista</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center bg-white p-2 sm:p-4 rounded-lg shadow-md div-proj mx-auto">
+              <div className="w-16 h-16 rounded-full mb-4"></div>
+              <p className="text-center text-sm">API de pagamento</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center bg-white p-2 sm:p-4 rounded-lg shadow-md div-proj mx-auto">
+              <div className="w-16 h-16 rounded-full mb-4"></div>
+              <p className="text-center text-sm">API de projeto universitário </p>
+            </div>
+          </div>
+        </div>
         {/* Conteúdo da seção Projetos */}
       </section>
       <section id="contato">
